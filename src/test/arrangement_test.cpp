48616cd30d6bf56ff0a4eda202d8c78a002a2db5
Eytlin <dartlevonchic@gmail.com>
2019-02-22 21:20:23 +0300

    Create arrangement_test.cpp

---
+++ src/test/arrangement_test.cpp
@@ -0,0 +1,32 @@
+#include "include/math.h"
+#include "include/vec2.h"
+#include "include/vec3.h"
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+using namespace cppmath;
+
+// clear && gcc -std=c++11 -w -g vec2.cpp vec3.cpp arrangement_test.cpp -lstdc++ -lm -o arrangement_test && cat test_arrange.txt | ./arrangement_test
+
+int main(int argc, char **argv) {
+	while (cin) {		
+		vec2 A, B, C, D;
+		cin >> A.x >> A.y;
+		cin >> B.x >> B.y;
+		cin >> C.x >> C.y;
+		cin >> D.x >> D.y;
+		
+		int test;
+		cin >> test;
+		
+		int result = math::arrangement(A, B, C, D);
+		if (result != test)
+			cout << "Missed for " << A << "; " << B << "; " << C << "; " << D << "; result = " << result << ", test = " << test << " MISS" << endl; 
+		else
+			cout << "Passed for " << A << "; " << B << "; " << C << "; " << D << "; result = " << result << ", test = " << test << endl; 
+	}
+	
+	return 0;
+};
