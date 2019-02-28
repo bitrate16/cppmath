56b421ccc984b11c06a966d082323a37318c8164
Eytlin <dartlevonchic@gmail.com>
2019-02-28 10:25:09 +0300

    Update fraction.h

--- src/include/fraction.h
+++ src/include/fraction.h
@@ -52,6 +52,18 @@ public:
 		if (q.is_zero())
 			throw std::runtime_error("divide by zero");
 		
+		if (p == q) {
+			w += 1;
+			p = 0;
+			q = 1;
+			return;
+		}
+		
+		if (p == 0) {
+			q = 1;
+			return;
+		}
+		
 		bigint gcd = bigint::gcd(p, q);
 		
 		if (gcd > 1) {
@@ -75,16 +87,49 @@ public:
 		return *this;
 	};
 	
-	fraction& operator*=(const fraction& f) {
-		if (q == w.q) {
-			
-		}
-		
+	fraction& operator+=(const fraction& f) {
 		w *= f.w;
-		p *= f.p;
-		q *= f.q;
+		
+		if (q == f.q) 
+			p += f.p;
+		else {
+			p += f.p * q;
+			q *= f.q;
+		}
 		
 		normalize();
 		return *this;
+	};
+	
+	fraction& operator-=(const fraction& f) {
+		w *= f.w;
+		
+		if (q == f.q) 
+			p -= f.p;
+		else {
+			p -= f.p * q;
+			q *= f.q;
+		}
+		
+		normalize();
+		return *this;
+	};
+	
+	fraction& operator-=(const fraction& f) {
+		w *= f.w;
+		
+		if (q == f.q) 
+			p -= f.p;
+		else {
+			p -= f.p * q;
+			q *= f.q;
+		}
+		
+		normalize();
+		return *this;
+	};
+	
+	fraction& operator/=(const fraction& f) {
+		
 	};
 };
